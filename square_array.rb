c4750741b908c1e0f57f325f701c07fdfa55c391
Elijah Adebiyi Oyeniyi <biyibama@gmail.com>
2018-12-09 01:54:13 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
-def square_array(array)
-  # your code here
-end
+square array lab
+{
+  numbers = [25,16,9,4]
+  
+  square_array(numbers)
+  # => [625,256,81,16]
+  
+  new_numbers = [5,4,3,2]
+  
+  square_array(new_numbers)
+  # => [25,16,9,2]
+  
+}
